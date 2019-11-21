1b6acd7bb4b6e6dea2b9f40e0a1d4e84344ba371
Agung Gunawan <lembagax@gmail.com>
2019-11-21 22:30:43 +0700

    Logout at sidebar

--- resources/views/main/_sidebar.blade.php
+++ resources/views/main/_sidebar.blade.php
@@ -32,6 +32,18 @@
                     </p>
                   </a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('logout') }}"
+                       onclick="event.preventDefault();
+                                     document.getElementById('logout-form').submit();">
+                        <i class="nav-icon fas fa-sign-out-alt"></i>
+                        {{ __('Logout') }}
+                    </a>
+
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
+                </li>
             </ul>
         </nav>
     </div>
