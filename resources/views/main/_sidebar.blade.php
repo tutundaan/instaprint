2bfc11c205f726e9dfcaa36d186285174bfbae25
Agung Gunawan <com.agunggunawan@gmail.com>
2020-02-01 18:53:22 +0700

    ganti label

--- resources/views/main/_sidebar.blade.php
+++ resources/views/main/_sidebar.blade.php
@@ -1,5 +1,5 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <a href="index3.html" class="brand-link">
+    <a href="/" class="brand-link">
         <img src="https://adminlte.io/themes/v3/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
         style="opacity: .8">
         <span class="brand-text font-weight-light"><strong>{{ config('app.name') }}</strong></span>
