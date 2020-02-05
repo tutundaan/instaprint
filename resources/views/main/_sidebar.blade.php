ddcb5fef3421463484407dd160e33014f5c27934
Agung Gunawan <com.agunggunawan@gmail.com>
2020-02-05 21:50:30 +0700

    hide admin dashboard di sidebar

--- resources/views/main/_sidebar.blade.php
+++ resources/views/main/_sidebar.blade.php
@@ -11,6 +11,7 @@
         </div>
         <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                @if(!Auth::user()->isAdmin())
                 <li class="nav-item">
                   <a href="{{ route('auth.dashboard.index') }}" class="nav-link {{ Request::is('auth/home') ? 'active' : false }}">
                     <i class="nav-icon fas fa-tachometer-alt"></i>
@@ -19,6 +20,7 @@
                     </p>
                   </a>
                 </li>
+                @endif
                 @if(Auth::user()->isAdmin() or Auth::user()->isManager() or Auth::user()->isSupervisor())
                 @if(Auth::user()->isAdmin())
                 <li class="nav-item has-treeview {{ (Request::is('auth/user*') or Request::is('auth/link-account*')) ? 'menu-open' : false }}">
