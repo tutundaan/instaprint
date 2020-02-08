3e8f8f7073d80b8524b02c873e7a88de899e6f92
Agung Gunawan <com.agunggunawan@gmail.com>
2020-02-08 18:35:29 +0700

    fix rating modal show

--- resources/views/auth/rating/_stars.blade.php
+++ resources/views/auth/rating/_stars.blade.php
@@ -1,13 +1,13 @@
 @if($employee->rating())
-<button type="button" class="btn btn-primary btn-xs" data-toggle="modal" data-target="#ratingEmployeeInfo">
+<button type="button" class="btn btn-primary btn-xs" data-toggle="modal" data-target="#ratingEmployeeInfo{{ $employee->id }}">
     @include('auth.rating._rate', ['rate' => $employee->rating() ])
 </button>
 
-<div class="modal fade" id="ratingEmployeeInfo" tabindex="-1" role="dialog" aria-labelledby="ratingEmployeeInfo" aria-hidden="true">
+<div class="modal fade" id="ratingEmployeeInfo{{ $employee->id }}" tabindex="-1" role="dialog" aria-labelledby="ratingEmployeeInfo{{ $employee->id }}" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="ratingEmployeeInfo">Rating Terakhir</h5>
+        <h5 class="modal-title" id="ratingEmployeeInfo{{ $employee->id }}">Rating Terakhir {{ $employee->formattedName() }}</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
